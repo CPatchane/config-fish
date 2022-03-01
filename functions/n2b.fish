649bc489a6df764c79f2126f8d89b1523ca2e3a1
CPatchane <code@patchane.com>
2022-03-01 14:54:07 +0100

    Improve n2b to handle branch removal if wanted

--- functions/n2b.fish
+++ functions/n2b.fish
@@ -1,19 +1,51 @@
-function n2b --description "Push npm build files (defined in package.json files property) to an orphan branch" --argument-names branch_name folder
+function n2b --description "Push npm build files (defined in package.json files property) to an orphan branch" --argument-names branchName folder
     if ! command -vq git; echo "Git is not installed"; return; end
     if ! command -vq npm; echo "npm is not installed"; return; end
     if ! command -vq tar; echo "tar is not installed"; return; end
-    if [ -z "$branch_name" ]; echo "Branch name required as first argument"; return; end
+    if [ -z "$branchName" ]; echo "Branch name required as first argument"; return; end
 
     test -n "$folder"; or set -l folder "./"
 
-    if git show-ref "refs/heads/$branch_name" --quiet;
-        printf "\n%s\n" "$branch_name branch already exists :/"
-        return
+    read -l -P "Git remote to push to? (origin) " remoteToPush
+    test -n "$remoteToPush"; or set -l remoteToPush "origin"
+
+    set -l remoteUrl (git remote get-url $remoteToPush)
+    string match -rq '.*[:/](?<host>[\\w-]*)\\/(?<repo>[\\w-]*)\\.git\\/?$' $remoteUrl
+
+    printf "\n%s\n" "> Checking repository..."
+
+    if git show-ref "refs/heads/$branchName" --quiet;
+        set -g localBranchExists 1
+        printf "\n%s" "Branch `$branchName` already exists locally."
+    else
+        set -g localBranchExists 0
     end
+
+    if git ls-remote -h --exit-code $remoteUrl $branchName &> /dev/null;
+        set -g remoteBranchExists 1
+        printf "\n%s" "Branch `$branchName` already exists on the remote."
+    else
+        set -g remoteBranchExists 0
+    end
+
+    if test $localBranchExists -eq 1; or test $remoteBranchExists -eq 1;
+        printf "\n\n"
+        read -l -P "Do you want to erase? " isOverwritten
+        switch $isOverwritten
+        case y yes Y YES
+            if test $localBranchExists -eq 1; git branch -qD $branchName; end
+            if test $remoteBranchExists -eq 1; git push -qd $remoteToPush $branchName; end
+        case '*'
+            printf "\n%s\n\n" "Aborted."
+            return
+        end
+    end
+
+    printf "\n%s\n" "> Checking repository... ✅"
 
     printf "\n%s" "> Creating the build branch..."
 
-    git checkout --orphan "$branch_name" --quiet
+    git checkout --orphan "$branchName" --quiet
     git reset .
 
     printf "\r%s\n" "> Creating the build branch... ✅"
@@ -42,17 +74,9 @@ function n2b --description "Push npm build files (defined in package.json files 
 
     printf "\r%s\n\n" "> Committing... ✅"
 
-    read -l -P "Git remote to push to? (origin) " remoteToPush
-    test -n "$remoteToPush"; or set -l remoteToPush "origin"
-
-    set -l remoteUrl (git remote get-url $remoteToPush)
-    string match -rq '.*[:/](?<host>[\\w-]*)\\/(?<repo>[\\w-]*)\\.git\\/?$' $remoteUrl
-
-    printf "\n"
-
     git push $remoteToPush HEAD
 
-    printf "\n\n%s\n" "Install it using: yarn add $pkgName@$host/$repo#$branch_name"
+    printf "\n\n%s\n" "Install it using: yarn add $pkgName@$host/$repo#$branchName"
     printf "\n%s\n" "PS: You're in a detached branch."
     printf "%s\n" "If you go back to the main branch, don't forget to install your dependencies back."
 end

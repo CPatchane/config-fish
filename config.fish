805700c58695a5d445f6cdfc40a41bd2751f972b
CPatchane <code@patchane.com>
2025-11-04 14:44:52 +0100

    Fix git prompt stateseparator for latest fish version
    Before it wasn't displayed between 'git' and the branch name, now it's displayed, so we remove it

--- config.fish
+++ config.fish
@@ -90,13 +90,13 @@ set __fish_git_prompt_char_dirtystate '  '
 set __fish_git_prompt_char_invalidstate '  '
 set __fish_git_prompt_char_stagedstate '  '
 set __fish_git_prompt_char_stashstate '  '
-set __fish_git_prompt_char_stateseparator ' '
+set __fish_git_prompt_char_stateseparator ' '
 set __fish_git_prompt_char_untrackedfiles '  '
 set __fish_git_prompt_char_upstream_ahead '  '
 set __fish_git_prompt_char_upstream_behind '  '
 set __fish_git_prompt_char_upstream_diverged '  '
 set __fish_git_prompt_char_upstream_equal '  '
-set __fish_git_prompt_char_upstream_prefix ' '
+set __fish_git_prompt_char_upstream_prefix ''
 
 #
 ### PROMPT
